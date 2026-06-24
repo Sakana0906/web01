6983b0d1753c61a88fdd8546cd7420e33e31b512
Kongphop684245059 <Kongphop.Onkham@gmail.com>
2026-06-24 11:51:35 +0700

    ส่งงาน workshop2_1

---
+++ oop02/Workshop2/workshop2_1.ts.ts
@@ -0,0 +1,10 @@
+let age: number = 70;
+
+console.log(`Age: ${age}`);
+if (age < 12){
+    console.log("Movie price: $5.00");
+} else if (age >= 12 && age <= 60){
+    console.log("Movie price: $10.00");
+} else {
+    console.log("Movie price: $7.00");
+}
