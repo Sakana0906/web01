14921242d65828c299033613a4804c2d40b1c580
Kongphop684245059 <Kongphop.Onkham@gmail.com>
2026-07-01 12:04:14 +0700

    Delete ws3_2.ts

--- ws3_2.ts
+++
@@ -1,9 +0,0 @@
-const calculatePrice = (price: number, discountPercent: number): number => {
-    const discountAmount = price * (discountPercent / 100);
-    const netPrice = price - discountAmount < 0 ? 0 : price - discountAmount;
-    return netPrice;
-}
-let price: number = 979;
-let discountPercent: number = 111;
-let discountedPrice: number = calculatePrice(price, discountPercent);
-console.log(`Price: ${price}  Discount: ${discountPercent}%\nThe discounted price is: ${discountedPrice}`);
