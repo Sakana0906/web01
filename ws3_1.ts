20bd03bc6f8fccaef22d27b6c92f018ce19fba41
Kongphop684245059 <Kongphop.Onkham@gmail.com>
2026-07-01 12:02:29 +0700

    ส่งงาน Workshop_3

---
+++ ws3_1.ts
@@ -0,0 +1,9 @@
+//function calculateArea(width: number, height: number): number {
+//    return width * height;
+//}
+//console.log(`The area is: ${calculateArea(8, 10)}`);
+
+const calculateArea = (width: number, height: number): number => {
+    return width * height;
+}
+console.log(`Width: 8  Height: 10\nThe area is: ${calculateArea(8, 10)}`);
